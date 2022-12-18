51ce53a70949cb29ae7005aae9f3934a44188921
Vitalik Prokopenko <v.prokopenko@internetdevels.com>
2022-12-18 16:36:44 +0200

    Feat: Stupid Twig Response

--- web/src/Controllers/HomeController.php
+++ web/src/Controllers/HomeController.php
@@ -4,18 +4,21 @@ namespace Vinvit\MVC\Controllers;
 
 use Vinvit\MVC\Core\Controller\Controller;
 use Vinvit\MVC\Core\Attributes\Route;
+use Vinvit\MVC\Core\Controller\Response\TwigResponse;
 
 class HomeController implements Controller {
 
     #[Route('get', '/')]
     public function index()
     {
-        return "hello world";
+        $response = new TwigResponse('home');
+        return $response->getResponse();
     }
 
     #[Route('get', '/{id}',["id" => "[0-9]+"])]
     public function number(int $id = 0)
     {
-        return "Number: " . $id;
+        $response = new TwigResponse('home', ['num' => $id]);
+        return $response->getResponse();
     }
 }
---
+++ web/src/Core/Controller/Response/Response.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Vinvit\MVC\Core\Controller\Response;
+
+interface Response {}
